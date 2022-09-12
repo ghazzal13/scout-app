cd94ad14a0681819972ab004a21f058ddadbbc74
ghazal <ahmed.ghazal365@gmail.com>
2022-09-12 12:47:36 +0200

    Internet Connection Checker

--- lib/layout/pages/layout_page.dart
+++ lib/layout/pages/layout_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:scout/layout/pages/settings_screen.dart';
+import 'package:scout/layout/widgets/reuse_widget.dart';
 
 import '../widgets/layout_widgets/attack_line.dart';
 import '../widgets/layout_widgets/defense_line.dart';
@@ -13,6 +15,24 @@ class LayoutPage extends StatefulWidget {
 }
 
 class _LayoutPageState extends State<LayoutPage> {
+  Future<void> netCheck() async {
+    print('''The statement 'this machine is connected to the Internet' is: ''');
+    final bool isConnected = await InternetConnectionChecker().hasConnection;
+    // ignore: avoid_print
+    print(
+      isConnected.toString(),
+    );
+    if (isConnected == false) {
+      showSnackBar(context, 'You Don\'t Have Internet Connection');
+    }
+  }
+
+  @override
+  void initState() {
+    netCheck();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
