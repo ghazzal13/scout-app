839a019a86f61aaf5ef631269b5906e6811ac1d1
ghazal <ahmed.ghazal365@gmail.com>
2022-11-15 08:54:05 +0200

    tages class

--- lib/layout/widgets/player_widgets/tages_data.dart
+++ lib/layout/widgets/player_widgets/tages_data.dart
@@ -1,16 +1,9 @@
-class CategoryTages {
-  static final all = <TagesData>[
-    TagesData(label: 'left foot', image: ''),
-    TagesData(label: 'right foot', image: ''),
-    TagesData(label: 'african', image: ''),
-    TagesData(label: 'north african', image: ''),
-    TagesData(label: 'less 25 y-old', image: ''),
-    TagesData(label: 'less 30 y-old', image: ''),
-  ];
-}
+class Chose {
+  final int id;
+  final String name;
 
-class TagesData {
-  late final String label;
-  late final String image;
-  TagesData({required this.label, required this.image});
+  Chose({
+    required this.id,
+    required this.name,
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scout/bloc_observer.dart';
 import 'package:scout/cubit/cubit.dart';
 import 'package:scout/cubit/states.dart';
 import 'package:scout/layout/auth/pages/login_page.dart';
@@ -11,6 +12,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -24,8 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-            create: (BuildContext context) => PlayersCubit()..getAllPosts()),
+        BlocProvider(create: (BuildContext context) => PlayersCubit()),
       ],
       child: BlocConsumer<PlayersCubit, PlayersStates>(
           listener: (context, state) {},
@@ -69,127 +70,5 @@ class MyApp extends StatelessWidget {
 // android   1:453196205507:android:d687f1222ce85d1d1cc176
 // ios       1:453196205507:ios:18deb9fdafe9ee041cc176
 // macos     1:453196205507:ios:18deb9fdafe9ee041cc176
+// */
 
-//  */
-/*
-
-import 'dart:convert';
-import 'package:flutter/material.dart';
-
-import 'package:flutter_tags/flutter_tags.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Tags Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: const MyHomePage(title: 'Flutter Tags'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-  late ScrollController _scrollViewController;
-
-  final List<String> _list = [
-    'left foot',
-    'right foot',
-    'african',
-    'north african',
-    'less 25 y-old',
-    'less 30 y-old',
-  ];
-
-  final bool _symmetry = false;
-  final bool _removeButton = true;
-  final bool _startDirection = false;
-  final bool _horizontalScroll = true;
-  final bool _withSuggesttions = false;
-  final int _count = 0;
-  final int _column = 0;
-
-  final String _itemCombine = 'withTextBefore';
-
-  final List _icon = [Icons.home, Icons.language, Icons.headset];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 1, vsync: this);
-    _scrollViewController = ScrollController();
-
-    _items = _list.toList();
-  }
-
-  late List _items;
-
-  final GlobalKey<TagsState> _tagStateKey = GlobalKey<TagsState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:
-       CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-              delegate: SliverChildListDelegate([
-            const Padding(
-              padding: EdgeInsets.all(20),
-            ),
-            _tags1,
-          ])),
-        ],
-      ),
-    );
-  }
-
-  Widget get _tags1 {
-    return Tags(
-      key: _tagStateKey,
-      symmetry: false,
-      columns: _column,
-      horizontalScroll: _horizontalScroll,
-      heightHorizontalScroll: 60 * (16 / 14),
-      itemCount: _items.length,
-      itemBuilder: (index) {
-        final item = _items[index];
-
-        return ItemTags(
-          key: Key(index.toString()),
-          index: index,
-          title: item,
-          active: false,
-          pressEnabled: true,
-          activeColor: Colors.blueGrey[600],
-          singleItem: false,
-          combine: ItemTagsCombine.withTextBefore,
-          textScaleFactor:
-              utf8.encode(item.substring(0, 1)).length > 2 ? 0.8 : 1,
-          textStyle: const TextStyle(
-            fontSize: 16,
-          ),
-          onPressed: (item) {
-            
-          },
-        );
-      },
-    );
-  }
-}
-*/
